b06e88556bb36e9edbfcabccc78420fd4649a679
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-01-17 21:07:22 -0700

    Revert "colored odd/even columns differently"
    This reverts commit 34748aba185a0701dba5ab82cf903301b8348050.

--- index.js
+++ index.js
@@ -46,25 +46,6 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-// function drawBricks() {
-//   for (let c = 0; c < brickColumnCount; c += 1) {
-//     for (let r = 0; r < brickRowCount; r += 1) {
-//       if (bricks[c][r].status === 1) {
-//         const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-//         const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-//         bricks[c][r].x = brickX;
-//         bricks[c][r].y = brickY;
-//         ctx.beginPath();
-//         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-//         ctx.fillStyle = '#0095DD';
-//         ctx.fill();
-//         ctx.closePath();
-//       }
-//     }
-//   }
-// }
-
-// trying to change row color
 function drawBricks() {
   for (let c = 0; c < brickColumnCount; c += 1) {
     for (let r = 0; r < brickRowCount; r += 1) {
@@ -75,12 +56,7 @@ function drawBricks() {
         bricks[c][r].y = brickY;
         ctx.beginPath();
         ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        // ctx.fillStyle = '#0095DD';
-        if (c % 2 === 0) {
-          ctx.fillStyle = '#0095DD';
-        } else {
-          ctx.fillStyle = '#FF0000';
-        }
+        ctx.fillStyle = '#0095DD';
         ctx.fill();
         ctx.closePath();
       }
