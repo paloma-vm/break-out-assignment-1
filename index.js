21d6a9fcd0992df98a0632d08a625d443e070c68
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-01-19 11:16:46 -0700

    added index.js
    trying to add index.js to main branch

---
+++ index.js
@@ -0,0 +1,205 @@
+
+      // JavaScript code goes here
+      const canvas = document.getElementById("myCanvas");
+      const ctx = canvas.getContext("2d");
+      //define x and y
+      let x = canvas.width / 2;
+      let y = canvas.height - 30;
+      let dx = 2;
+      let dy = -2;
+      const ballRadius = 10;
+      const paddleHeight = 10;
+      const paddleWidth = 75;
+      let paddleX = (canvas.width - paddleWidth) / 2;
+      let rightPressed = false;
+      let leftPressed = false;
+      //false because at the beginning the control buttons are not pressed
+      const brickRowCount = 3;
+      const brickColumnCount = 5;
+      const brickWidth = 75;
+      const brickHeight = 20;
+      const brickPadding = 10;
+      const brickOffsetTop = 30;
+      const brickOffsetLeft = 30;
+      const bricks = [];
+      for (let c = 0; c < brickColumnCount; c++) {
+        bricks[c] = [];
+        for (let r = 0; r < brickRowCount; r++) {
+          bricks[c][r] = { x: 0, y: 0, status: 1 };
+        }
+      }
+      let score = 0;
+      let lives = 3;
+
+      function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+        ctx.fillStyle = "#0095DD";
+        ctx.fill();
+        ctx.closePath();
+      }
+
+      function drawBricks() {
+        for (let c = 0; c < brickColumnCount; c++) {
+          for (let r = 0; r < brickRowCount; r++) {
+            if (bricks[c][r].status === 1) {
+              const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
+              const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
+              bricks[c][r].x = brickX;
+              bricks[c][r].y = brickY;
+              ctx.beginPath();
+              ctx.rect(brickX, brickY, brickWidth, brickHeight);
+              ctx.fillStyle = "#0095DD";
+              ctx.fill();
+              ctx.closePath();
+            }
+          }
+        }
+      }
+
+
+ 
+      function draw() {
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBricks();
+        drawBall();
+        drawPaddle();
+        drawScore();
+        drawLives();
+        collisionDetection();
+       
+        // bounce off top wall by reversing y-axis movement
+        if (y + dy < 0) {
+          dy = -dy;
+        }
+        //bounce off bottom wall
+        if (y + dy > canvas.height) {
+          dy = -dy;
+        }
+        //bounce off left and right
+        if (x + dx > canvas.width || x + dx < 0) {
+          dx = -dx;
+        }
+
+        if (y + dy > canvas.height || y + dy < 0) {
+          dy = -dy;
+        }
+      
+        //adjusting for radius of ball so it doesn't 'sink' into the wall
+        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+          dx = -dx;
+        }
+        //if ball hits bottom wall
+        if (y + dy < ballRadius) {
+          dy = -dy;
+        } else if (y + dy > canvas.height - ballRadius) {
+            if (x > paddleX && x < paddleX + paddleWidth) {
+              dy = -dy;
+            } else {
+                lives--;
+                if (!lives) {
+                  alert("GAME OVER");
+                  document.location.reload();
+                } else {
+                  x = canvas.width / 2;
+                  y = canvas.height - 30;
+                  dx = 2;
+                  dy = -2;
+                  paddleX = (canvas.width - paddleWidth) / 2;
+                }
+
+            }
+        }
+
+
+        //checking to see if the RT or LT keys are pressed
+        if (rightPressed) {
+          paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+        } else if (leftPressed) {
+          paddleX = Math.max(paddleX - 7, 0);
+        }
+
+        x += dx;
+        y += dy;
+        // causes the draw() function to call itself over and over again
+        // the browser controls the frame rate
+        requestAnimationFrame(draw); 
+      }
+
+      //event listeners for control buttons
+      document.addEventListener("keydown", keyDownHandler, false);
+      document.addEventListener("keyup", keyUpHandler, false);
+      document.addEventListener("mousemove", mouseMoveHandler, false);
+
+      function mouseMoveHandler(e) {
+        const relativeX = e.clientX - canvas.offsetLeft;
+        if (relativeX > 0 && relativeX < canvas.width) {
+          paddleX = relativeX - paddleWidth / 2;
+        }
+      }
+
+
+      function keyDownHandler(e) {
+        if (e.key === "Right" || e.key === "ArrowRight") {
+          rightPressed = true;
+        } else if (e.key === "Left" || e.key === "ArrowLeft") {
+          leftPressed = true;
+        }
+      }
+
+      function keyUpHandler(e) {
+        if (e.key === "Right" || e.key === "ArrowRight") {
+          rightPressed = false;
+        } else if (e.key === "Left" || e.key === "ArrowLeft") {
+          leftPressed = false;
+        }
+      }
+      function collisionDetection() {
+        for (let c = 0; c < brickColumnCount; c++) {
+          for (let r = 0; r < brickRowCount; r++) {
+            const b = bricks[c][r];
+            if (b.status === 1) {
+              if (
+                x > b.x &&
+                x < b.x + brickWidth &&
+                y > b.y &&
+                y < b.y + brickHeight
+              ) {
+                dy = -dy;
+                b.status = 0;
+                score++;
+                if (score === brickRowCount * brickColumnCount) {
+                  alert("YOU WIN, CONGRATULATIONS!");
+                  document.location.reload();
+                }
+              }
+            }
+          }
+        }
+      }
+
+      
+
+      function drawScore() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText(`Score: ${score}`, 8, 20);
+      }
+
+      function drawLives() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "#0095DD";
+        ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
+}
+
+
+      draw();
+      
