0c917a7e81f1d6bbd1e7303203ef7b3a202e692e
paloma-vm <paloma.valdez-marsh@students.dominican.edu>
2023-01-19 11:05:20 -0700

    merging assigmnent-2
    want main branch to have linted index.js in it

--- index.js
+++ index.js
@@ -46,25 +46,6 @@ function drawPaddle() {
   ctx.closePath();
 }
 
-// function drawBricks() {
-//     for (let c = 0; c < brickColumnCount; c += 1) {
-//       for (let r = 0; r < brickRowCount; r += 1) {
-//         if (bricks[c][r].status === 1) {
-//           const brickX = c * (brickWidth + brickPadding) + brickOffsetLeft;
-//           const brickY = r * (brickHeight + brickPadding) + brickOffsetTop;
-//           bricks[c][r].x = brickX;
-//           bricks[c][r].y = brickY;
-//           ctx.beginPath();
-//           ctx.rect(brickX, brickY, brickWidth, brickHeight);
-//           ctx.fillStyle = '#0095DD';
-//           ctx.fill();
-//           ctx.closePath();
-//         }
-//       }
-//     }
-//   }
-
-// each row a different color
 function drawBricks() {
     for (let c = 0; c < brickColumnCount; c += 1) {
       for (let r = 0; r < brickRowCount; r += 1) {
@@ -75,14 +56,7 @@ function drawBricks() {
           bricks[c][r].y = brickY;
           ctx.beginPath();
           ctx.rect(brickX, brickY, brickWidth, brickHeight);
-        //   ctx.fillStyle = '#0095DD';
-          if (r / 2 === 1) {
           ctx.fillStyle = '#0095DD';
-          } else if (r % 2 === 0) {
-            ctx.fillStyle = '#FF0000';
-          } else {
-            ctx.fillStyle = '#a89e32';
-          }
           ctx.fill();
           ctx.closePath();
         }
